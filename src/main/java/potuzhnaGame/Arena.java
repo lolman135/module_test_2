99e805dd539b3dbaf5a01d9ccd037ee1a0ec6293
Kyrylo Kusovsky <kkirilll2006@gmail.com>
2024-12-18 12:33:06 +0200

    Add obserber support for Arena

--- src/main/java/potuzhnaGame/Arena.java
+++ src/main/java/potuzhnaGame/Arena.java
@@ -1,21 +1,39 @@
 package potuzhnaGame;
 
+import potuzhnaGame.observer.Observer;
 import potuzhnaGame.player.Character;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Arena {
+public class Arena{
 
     private List<Character> characters = new ArrayList<>();
+    private List<Observer> observers = new ArrayList<>();
+
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObserver(String message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
 
     public void addCharacter(Character character) {
         characters.add(character);
+        notifyObserver("New character added: " + character.getName());
     }
 
     public void attack(Character attacker, Character target, int hp) {
         if (characters.contains(attacker) && characters.contains(target) && attacker != target) {
             target.takeDamage(hp);
+            System.out.println(attacker.getName() + " attacks " + target.getName());
         } else {
             throw new IllegalArgumentException("Either attacker or target is not in the arena or they are the same character.");
         }
--- src/main/java/potuzhnaGame/Main.java
+++ src/main/java/potuzhnaGame/Main.java
@@ -1,4 +1,26 @@
 package potuzhnaGame;
 
+import potuzhnaGame.observer.PlayerAddObserver;
+import potuzhnaGame.player.Character;
+
 public class Main {
+    public static void main(String[] args) {
+        Arena arena = new Arena();
+        PlayerAddObserver observer = new PlayerAddObserver("Observer1");
+        arena.addObserver(observer);
+
+        CharacterFactory characterFactory = new CharacterFactory();
+
+        Character warrior = characterFactory.createCharacter("warrior", "Strong Warrior", 100, 0, 0, 20, arena);
+        Character warlock = characterFactory.createCharacter("warlock", "Mighty Warlock", 80, 1, 1, 15, arena);
+        Character archer = characterFactory.createCharacter("archer", "Quick Archer", 70, 2, 2, 25, arena);
+
+        arena.addCharacter(warrior);
+        arena.addCharacter(warlock);
+        arena.addCharacter(archer);
+
+        warrior.attack(warlock);
+        warlock.attack(archer);
+        archer.attack(warrior);
+    }
 }
